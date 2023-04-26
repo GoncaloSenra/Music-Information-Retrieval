af6a0bceab9483c8de24761b0a1c4523a110da10
Gonçalo Senra <goncalosenra@gmail.com>
2023-04-26 16:06:13 +0100

    Add features

---
+++ TP2.py
@@ -0,0 +1,16 @@
+
+import librosa #https://librosa.org/    #sudo apt-get install -y ffmpeg (open mp3 files)
+import librosa.display
+import librosa.beat
+import sounddevice as sd  #https://anaconda.org/conda-forge/python-sounddevice
+import warnings
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
