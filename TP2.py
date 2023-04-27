ef00f12cb8e232917265ad857b01789c9311a49d
Gonçalo Senra <goncalosenra@gmail.com>
2023-04-27 16:55:38 +0100

    Feature extraction

--- TP2.py
+++ TP2.py
@@ -22,7 +22,7 @@ def main():
 
     #print(featuresN)
     #print(nomesMusicas)
-    #np.savetxt('featuresN.csv', featuresN, delimiter=',') #salvar matriz normalizada num excel
+    np.savetxt('featuresN.csv', featuresN, delimiter=',') #salvar matriz normalizada num excel
     extrairFeatures(nomesMusicas)
 
 #correr a matriz linha a linha, obter o max e min de cada coluna e aplicar a formula caso n sejam iguais
@@ -41,19 +41,25 @@ def normalizar(matriz):
     return matrizN
     
 def stats(feat):
-    return np.array[(np.mean(feat), np.std(feat), sc.skew(feat), sc.kurtosis(feat), np.median(feat), np.max(feat), np.min(feat))]
+    return np.array([np.mean(feat), np.std(feat), sc.skew(feat), sc.kurtosis(feat), np.median(feat), np.max(feat), np.min(feat)])
 
 def extrairFeatures(nomesMusicas):
     sr = 22050
     windowL = frameL = 92.88
     hopL = 23.22
-    feqMin = 20
+    freqMin = 20
     freqMax = 11025
     
     nomesQ = np.array(['Q1','Q2','Q3','Q4'])
 
+    statistics = np.zeros((900, 190))
+
+    num_music = 0
+    num_stats = 7
+    num = 1
     for queryName in nomesQ:
         for music in os.listdir('MER_audio_taffc_dataset\\' + queryName):
+            print('Music: '+str(num) + ' / 900')
             #nomeMus = nomeMus[1:-1]
             #caminho = 'MER_audio_taffc_dataset\\' + queryName +'\\' + nomeMus +'.mp3'
             #print(caminho)
@@ -66,30 +72,80 @@ def extrairFeatures(nomesMusicas):
 
             #mfcc
             mfcc = librosa.feature.mfcc(y = y[0], n_mfcc = 13, hop_length = int(hopL))
+            
+            #mfcc = mfcc[0, :]
+            i = 0
             for i in range(mfcc.shape[0]):
-                pass
+                statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(mfcc[i, :])
                 
-
+            #print(i)
+            i += 1
             #spectral centroid
             specCen = librosa.feature.spectral_centroid(y = y[0], hop_length = int(hopL))
-
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(specCen[0, :])
+            
+            #print(i)
+            i += 1
             #spectral bandwidth
             specBand = librosa.feature.spectral_bandwidth(y=y[0])
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(specBand[0, :])
 
+            #print(i)
+            i += 1
             #spectral costrast
             spectralContr = librosa.feature.spectral_contrast(y=y[0])
+            j = i
+            for i in range(j, spectralContr.shape[0] + j):
+                statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(spectralContr[i - j, :])
+            
 
+            #i = j
+            #print(i)
+            i += 1
             #spectral flatness
             spectralFlat = librosa.feature.spectral_flatness(y=y[0])
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(spectralFlat[0, :])
 
+            #print(i)
+            i += 1
             #spectral rolloff
             spectralRoll = librosa.feature.spectral_rolloff(y=y[0])
-
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(spectralRoll[0, :])
 
             #features temporais
 
+            #print(i)
+            i += 1
+            #yin
+            F0 = librosa.yin(y=y[0], fmin=freqMin, fmax=freqMax)
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(F0)
+
+            #print(i)
+            i += 1
+            #RMS
+            RMS = librosa.feature.rms(y=y[0])
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(RMS[0, :])
+
+            #print(i)
+            i += 1
+            #zero crossing rate
+            zero_crossing_rate = librosa.feature.zero_crossing_rate(y=y[0])
+            statistics[num_music, i * num_stats : i * num_stats + num_stats] = stats(zero_crossing_rate[0, :])
 
             #outras features 
+
+            #print(i)
+            i += 1
+            #tempo
+            tempo = librosa.feature.rhythm.tempo(y=y[0])
+            statistics[num_music, i * num_stats] = tempo[0]
+
+            num_music = num_music + 1
+            num+=1
+
+    statisticsN = normalizar(statistics)
+
+    np.savetxt('statisticsN.csv', statisticsN, delimiter=',')
 
 
 
